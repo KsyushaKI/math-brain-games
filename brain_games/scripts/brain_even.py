30b8f56a9ef4eb4895779444efe1f723bb5a284f
KsyushaKI <oksanaakadem39@gmail.com>
2022-12-29 21:51:12 +0300

    step 5: create game-even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from brain_games.logic_brain_even import brain_even, welcome_user, greeting_user, start_game
+
+
+def main():
+    welcome_user()
+    greeting_user()
+    start_game()
+    brain_even()
+
+
+if __name__ == '__main__':
+    main()
